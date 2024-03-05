ed21b0a6cbef34edf55e7945783343487e31ced8
Hammered-Cucumber <147238296+Hammered-Cucumber@users.noreply.github.com>
2024-03-05 19:06:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 let button = document.querySelector('#connect');
 let sendForm = document.getElementById('send-form');
-let inputField = document.getElementById('input');
+const terminalContainer = document.getElementById('input');
 
-connect.addEventListener('click', () => {
-  terminal.connect().
-      then(() => {
-        deviceNameLabel.textContent = terminal.getDeviceName() ?
-            terminal.getDeviceName() : defaultDeviceName;
-      });
+button.addEventListener('pointerup', function(event) {
+  // Call navigator.bluetooth.requestDevice
+
+  navigator.bluetooth.requestDevice({
+  acceptAllDevices: true,
+  optionalServices: ['battery_service'] // Required to access service later.
+})
+.then(device => { /* … */ })
+.catch(error => { console.error(error); });
+
+  navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+.then(device => {
+  // Human-readable name of the device.
+  console.log(device.name);
+
+  // Attempts to connect to remote GATT Server.
+  return device.gatt.connect();
+})
+.then(server => { /* … */ })
+.catch(error => { console.error(error); });
 });
+//aboba
+const send = (data) => {
+  terminal.send(data).
+      then(() => logToTerminal(data, 'out')).
+      catch((error) => logToTerminal(error));
+};
 
 sendForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -18,3 +38,9 @@ sendForm.addEventListener('submit', (event) => {
   inputField.value = '';
   inputField.focus();
 });
+
+const send = (data) => {
+  terminal.send(data).
+      then(() => input(data, 'out')).
+      catch((error) => input(error));
+};
